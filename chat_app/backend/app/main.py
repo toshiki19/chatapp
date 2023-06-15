6c83f07ef14bb4ea95e906bc8d0ab6ec8306e525
toshiki19 <toshiki.u.19@gmail.com>
2023-06-15 12:18:18 +0900

    api keyの作成

--- chat_app/backend/app/main.py
+++ chat_app/backend/app/main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+from app import config  # さっきのAPI KEYをインポートしておく
 
 app = FastAPI()
+
+# CORSの設定を行っています
+# 今回これはおまじないだと思ってください
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.post('/talk')
+async def get_talk(query: str = Body(..., embed=True)):
+
+    api_url = 'https://api.a3rt.recruit.co.jp/talk/v1/smalltalk'
+    # form-data形式だと、このような記述になります
+    form = {
+        'apikey': (None, config.RECRUIT_API_KEY),  # このようにAPI KEYを使います
+        'query': (None, query),
+    }
+
+    # postでリクエストを送ります
+    res = requests.post(api_url, files=form)
+
+    # レスポンスをJSONにdecodeして、そのまま返します
+    res_json = res.json()
+
+    return res_json
 
 
 @app.get('/')
